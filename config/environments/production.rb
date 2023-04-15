7ac2f64aea375f9f1ef467621a37ddfc261c5d07
niallc18 <niallcanning18@gmail.com>
2023-04-15 18:04:55 +0000

    mailer bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = {host: "11bc38c87f79421ca0031a3b952e0307.vfs.cloud9.us-east-1.amazonaws.com", protocol: "https"}
+  config.action_mailer.default_url_options = {host: "cyberlearnv1.herokuapp.com", protocol: "https"}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp

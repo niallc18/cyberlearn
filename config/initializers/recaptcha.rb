1485fdd133f4f63a939141e1c760441f78660098
niallc18 <niallcanning18@gmail.com>
2023-05-15 00:27:21 +0000

    fix

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 Recaptcha.configure do |config|
   if Rails.env.production?
     config.site_key  = ENV['RECAPTCHA_SITE_KEY'] 
-    config.secret_key = ENV['RECAPTCHA_SITE_KEY']
+    config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
   else
     config.site_key  = Rails.application.credentials.dig(:recaptcha, :site_key) 
     config.secret_key = Rails.application.credentials.dig(:recaptcha, :secret_key)
